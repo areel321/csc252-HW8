5641ce2f323a3d085b9ef23dcf6c36137d757a96
areel321 <112980267+areel321@users.noreply.github.com>
2023-11-12 17:03:35 -0500

    commit hwk8-code

--- hwk8-code.py
+++ hwk8-code.py
@@ -7,37 +7,32 @@ References: N/A
 import csv
 
 def createGraph():
+    #DOCSTRING
     neighborGraph = {}
     colorGraph = {}
-    #TODO: create a graph from csv file
+    #create a graph from csv file
     with open('states.csv', newline='') as csvfile:
         statesreader = csv.reader(csvfile)
         for row in statesreader:
-            #print(row[0])
-            #print(row[1])
             statesList = row[1].split(";")
             neighborGraph[row[0]] = statesList
             colorGraph[row[0]] = -1
-    print(neighborGraph)
     return neighborGraph, colorGraph
 
 
 def randomStart(neighborGraph, colorGraph):
-    #TODO: color in the graph from a random start point
+    #DOCSTRING
+    #color in the graph from a random start point
     #initiate number of colors needed as 0
     numColors = 0
     #create a list of states we havent covered
     statesUnColored = []
     for k in neighborGraph:
         statesUnColored.append(k)
-
     #go through the list removing states as we color them
-
     while statesUnColored:
- 
         #get the next state
         state = statesUnColored[0]
-        print(state)
         #color it in as color 0
         colorGraph[state] = 0
         #check the neighbors
@@ -45,39 +40,35 @@ def randomStart(neighborGraph, colorGraph):
             if v != '':
                 if colorGraph[v] == colorGraph[state]:
                     colorGraph[state] +=1
-                    numColors+=1
-
-        
+                    if colorGraph[state] >= numColors:
+                        numColors = colorGraph[state]
         statesUnColored.remove(state)
-        
-            
-
-
-
-    return neighborGraph, colorGraph, numColors+1
-
-
+    return colorGraph, numColors
 
 
 def mostNeighbors(neighborGraph, colorGraph):
-    #TODO: color in the graph starting with the most neighbors
+    #DOCSTRING
+    #color in the graph starting with the most neighbors
     #initiate number of colors needed as 0
     numColors = 0
     #find the state with the most neighbors
     statesUnColored = []
     numNeighbors = 0
     key = ''
-    for k in neighborGraph:
-        newNum = len(neighborGraph[k])
-        if numNeighbors < newNum:
-            numNeighbors = newNum
-            key = k
-            statesUnColored.append(key)
-
+    #FIXHERE
+    newdict = neighborGraph.copy()
+    for i in range(0,50):
+        for k in newdict:
+            newNum = len(newdict[k])
+            if k not in statesUnColored:
+                if numNeighbors < newNum:
+                    numNeighbors = newNum
+                    key = k
+        numNeighbors = 0
+        statesUnColored.append(key)
     while statesUnColored:
         #get the next state
         state = statesUnColored[0]
-        print(state)
         #color it in as color 0
         colorGraph[state] = 0
         #check the neighbors
@@ -85,30 +76,28 @@ def mostNeighbors(neighborGraph, colorGraph):
             if v != '':
                 if colorGraph[v] == colorGraph[state]:
                     colorGraph[state] +=1
-                    numColors+=1
+                    if colorGraph[state] >= numColors:
+                        numColors = colorGraph[state]
         statesUnColored.remove(state)
-
-    return neighborGraph, colorGraph, numColors+1
-
+    return colorGraph, numColors
 
 
 def main():
-    #TODO read in csv file
+    #DOCSTRING
+    #read in csv file
     neighborGraph, colorGraph = createGraph()
-    #TODO prompt user to select strategy
+    #prompt user to select strategy
     print("We have a dataaset of states to color in, would you like to start with a random start or the state with the most neighbors?")
     strategy = input("Enter R for random start or M for most neighbors:")
-    #TODO run selected method
+    #run selected method
     if strategy == "R":
-        neighborGraph, colorGraph, numColors = randomStart(neighborGraph, colorGraph)
+        colorGraph, numColors = randomStart(neighborGraph, colorGraph)
     elif strategy == "M":
-        neighborGraph, colorGraph, numColors = mostNeighbors(neighborGraph, colorGraph)
+        colorGraph, numColors = mostNeighbors(neighborGraph, colorGraph)
     else:
-        print("invalid input")
-    #TODO return results in readable way
-    print("The maximum color is: ", numColors, "the color graph is: ", colorGraph)
-
-
+        return print("invalid input")
+    #return results in readable way
+    return print("The maximum color is:", numColors, "the color graph is:", colorGraph)
 
 
 if __name__ == '__main__':
